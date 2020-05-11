817da131f8bb51d6cf17afdc1f01a64fcbbbd0d9
MrFacysus <52569856+MrFacysus@users.noreply.github.com>
2020-05-11 11:33:54 +0200

    v1.0.0
    Give it a dictionary and it will give you 5-word sentences that might get you newer ideas

---
+++ Idea Generator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Idea_Generator
+{
+    class Program
+    {
+        static string Output(string text)
+        {
+            string outpot = "";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
+
+            return outpot;
+        }
+        static void Gen(string[] Dict)
+        {
+            string text = "";
+            Random rand = new Random();
+            for (int I = 0; I < 10; I++)
+            {
+                for (int J = 0; J < 5; J++)
+                {
+                    text = text + Dict[rand.Next(0, Dict.Length)] + " ";
+                }
+                Output(text);
+                text = "";
+            }
+            Ret(Dict);
+        }
+        static void Ret(string[] Dict)
+        {
+            Output("\n Click anything to generate 10 ideas \n");
+            Console.ReadKey();
+            Gen(Dict);
+        }
+        static void Main(string[] args)
+        {
+            string text;
+            string[] Dictionary;
+
+            Output("Please input what word you want to use seperated by ',' character \n");
+            text = Console.ReadLine();
+            Dictionary = text.Split(',');
+
+            Ret(Dictionary);
+        }
+    }
+}
